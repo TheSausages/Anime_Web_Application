824fd242b7b1e454529639bb2ce64057ee2078ef
TheSausage <kziejlo@gmail.com>
2023-09-21 22:42:57 +0200

    Upgraded source compatibility to 21, upped dependency versions

--- services/Anime_Service/src/test/java/anime/app/anime/SpringDatabaseCommonApplicationTests.java
+++ services/Anime_Service/src/test/java/anime/app/anime/SpringDatabaseCommonApplicationTests.java
@@ -2,9 +2,7 @@ package anime.app.anime;
 
 import anime.app.dbbase.integrationtests.DatabaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringDatabaseCommonApplicationTests extends DatabaseIntegrationTest {
 
     @Test
--- services/Gateway/src/test/java/anime/app/gateway/SpringDatabaseCommonApplicationTests.java
+++ services/Gateway/src/test/java/anime/app/gateway/SpringDatabaseCommonApplicationTests.java
@@ -2,9 +2,7 @@ package anime.app.gateway;
 
 import anime.app.base.integrationtests.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringDatabaseCommonApplicationTests extends BaseIntegrationTest {
 
     @Test
